6d925a13a8bfe87c7acbcc9f6c8badd493e362bc
Matt Calder <calderm@usc.edu>
2020-04-01 16:00:19 -0700

    updates to dns scripts

--- scripts/bulkreverse.sh
+++ scripts/bulkreverse.sh
@@ -4,6 +4,6 @@ while read ip; do
         continue
     fi
 
-    echo "$ip $(dig 8.8.8.8 -x $ip +short | tr '\n' ',')"
+    echo "$ip $(dig @8.8.8.8 -x $ip +short | tr '\n' ',')"
     sleep 0.1
 done < "${1:-/dev/stdin}"
